5610c68cfab0aefb967c8608385a9fa116c57d05
dimkasta <dimkasta@dimkasta.net>
2015-07-11 18:03:21 +0100

    Update RESTAPI.php

--- RESTAPI.php
+++ RESTAPI.php
@@ -2,38 +2,30 @@
 	//REST API Helper class
 	class RESTAPI {
 		
-		protected $restObject;
+		protected $messages;
+		protected $data;
+		protected $success;
 		
 		public function __construct() {
-			$this->restObject = new stdClass();
-			$this->restObject->errors = new stdClass();
-			$this->restObject->messages = new stdClass();
-			$this->restObject->data = new stdClass();
-			$this->restObject->success false;
+			$this->messages = new stdClass();
+			$this->data = new stdClass();
+			$this->success false;
 		}
 		
 		public function addMessage($element, $message) {
-			$this->restObject->messages[$element] = $message;
-		}
-		
-		public function addError($element, $message) {
-			$this->restObject->errors[$element] = $message;
+			$this->messages[$element] = $message;
 		}
 		
 		public function addData($element, $value) {
-			$this->restObject->data[$element] = $value;
+			$this->data[$element] = $value;
 		}
 		
-		public function setStatuc($status) {
-			$this->restObject->success = $status;
+		public function setStatuc($value) {
+			$this->success = $value;
 		}
-		
-		public function getObject() {
-			return $this->restObject;
-		}
-		
+
 		public function __toString(){
 			header('Content-Type: application/json');
-			echo json_encode($this->restObject);
+			echo json_encode($this);
 		}
 ?>
